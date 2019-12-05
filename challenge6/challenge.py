8c640a86e9997a0ed4d6835d80b8044c6e6238f5
Yao-Phoenix <493867456@qq.com>
2019-12-05 11:27:59 +0800

    excel

---
+++ challenge6/challenge.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from openpyxl import load_workbook, Workbook
+import datetime
+
+wb = load_workbook('courses.xlsx')
+students_sheet = wb['students']
+time_sheet = wb['time']
+
+def combine():
+    combine_sheet = wb.create_sheet(title='combine')
+    combine_sheet.append(['', '', '', ''])
+    for stu in students_sheet.values:
+        if stu[2] != '':
+            for time in time_sheet.values:
+                if stu[1] == time[1]:
+                    combine_sheet.append(list(stu) + [time[2]])
+
+    wb.save('courses.xlsx')
+
+def split():
+    combine_sheet = wb['combine']
+    split_name = []
+    for item in combine_sheet.values:
+        if item[0] != '':
+            split_name.append(item[0].strftime("%Y"))
+
+    for name in set(split_name):
+        wb_temp = Workbook
+        wb_temp.remove(wb_temp.active)
+        ws = wb_temp.create_sheet(title=name)
+        ws.append(['', '', '', ''])
+        for item_by_year in combine_sheet.values:
+            if item_by_year[0] != '':
+                if item_by_year[0].strftime("%Y") == name:
+                    ws.append(item_by_year)
+        wb_temp.save = ('{}.xisx'.format(name))
+
+if __name__ == '__main__':
+    combine()
+    split()
+
