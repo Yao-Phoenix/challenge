79afb12e048d24896d4e79f1e03de29d64a2a6d8
Yao-Phoenix <493867456@qq.com>
2019-12-13 14:57:26 +0800

    receive MongoDB

--- challenge13/getrank.py
+++ challenge13/getrank.py
@@ -6,26 +6,24 @@ def get_rank(user_id):
     client = MongoClient()
     db = client.shiyanlou
     contests = db.contests 
-    scores = 0
-    submit_times = 0
+    data= {}
     for item in contests.find():
-        data = {item['user_id']: [item['score'], item['submit_time']]}
-        scores += data[item['user_id']][0]
-        submit_times += data[item['user_id']][1]
-        rank_list = sorted(data.values(), key = lambda x: (x[0],x[1]))
+        if data.get(item['user_id']):
+            data[item['user_id']][0] += item['score']
+            data[item['user_id']][1] += item['submit_time']
+        else:
+            data[item['user_id']] = [item['score'], item['submit_time']]
+    rank_list = sorted(data.values(), key = lambda x: (-x[0],x[1]))
     for i, j in enumerate(rank_list):
-        rank = i
-        score = data[[k for k, v in data.items() if v==j]][0]
-        submit_time = data[[k for k, v in data.items() if v==j]][1]
-    return rank, score, submit_time
+        data[[k for k, v in data.items() if v==j][0]].insert(0, i+1)
+    return tuple(data[user_id])
 
 if __name__ == '__main__':
     try:
         user_id = int(sys.argv[1])
+        print(get_rank(user_id))
     except:
-        print('Parameter Error')
+        print("NOTFOUND")
         exit()
-
-    print(get_rank(user_id))
 
 
